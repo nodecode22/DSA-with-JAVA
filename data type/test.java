f57a50f5bef70979ea4073b31b95d20b4bd26305
nodecode22 <saurabhdoforyou@gmail.com>
2026-08-16 21:55:13 +0530

    started DSA from today

---
+++ data type/test.java
@@ -0,0 +1,22 @@
+public class test{
+public static void main(String[] arg){
+
+    System.out.println("Hello, World!");
+    int a =10;
+    int b=20;
+
+    System.out.println(a);
+    System.out.println(b);
+
+    String name="Tony Stark";
+    System.out.println(name);
+     
+    a=b;
+    System.out.println(a);
+    
+    
+    
+
+
+}
+}
