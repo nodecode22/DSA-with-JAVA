75b6889bb24d4860a726c31674cc92a33bc4edf3
nodecode22 <saurabhdoforyou@gmail.com>
2026-08-16 22:51:41 +0530

    just complted the java data type lecture and code on that

---
+++ data type/javabasics/javabasics.java
@@ -0,0 +1,28 @@
+public class javabasics{
+public static void main(String[] arg){
+
+    byte b=8;
+    System.out.println(b);
+
+    char c='s';
+    System.out.println(c);
+
+    boolean state=true;
+    System.out.println(state);
+
+    float n = 432.12f;
+    System.out.println(n);
+
+    int x=123;
+    System.out.println(x);
+
+    //long double for more larger values
+
+    short g=234;
+    System.out.println(g);
+    
+
+    
+    
+}
+}
